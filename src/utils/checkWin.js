e0db99b9f29d3c950b347099ba8d19b9f0b081ee
Mike <michgoldennathan17@gmail.com>
2022-03-26 12:09:55 +0100

    Create checkwin logic with winning positions

---
+++ src/utils/checkWin.js
@@ -0,0 +1,17 @@
+import { WINNING_POSITIONS } from "constant";
+
+export const checkWin = (filledPositions, currentPlayer) => {
+  const filtPlayerPositions = Object.entries(filledPositions).filter(
+    (pos) => pos[1] === currentPlayer
+  );
+  const objFiltPlayerPositions = Object.fromEntries(filtPlayerPositions);
+  let isWinnerFound = false;
+  WINNING_POSITIONS.forEach((pos) => {
+    if (!isWinnerFound) {
+      isWinnerFound = pos.every((posInner) => objFiltPlayerPositions[posInner]);
+      console.log({ isWinnerFound });
+    }
+  });
+
+  return isWinnerFound;
+};
